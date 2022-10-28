3bab90b6b407191c8a2d4cac4633f982bb62518c
shk <shk@git.team-x.ir>
2022-10-28 14:54:23 +0330

    grant gps permission and current location

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,29 +1,59 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
     lateinit var binding: ActivityMainBinding
+    private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        binding.imgGPS.setOnClickListener {
+            if (isLocationPermissionGranted())
+                checkGPS()
+        }
+
+        binding.imgRecordGPS.setOnClickListener {
+            if (isLocationPermissionGranted())
+                fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location: Location? ->
+                        Toast.makeText(
+                            this@MainActivity,
+                            "${location?.latitude}",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -36,9 +66,55 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             .target(
                 tehran
             )
-            .zoom(10f)
+            .zoom(11f)
             .build()
 
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
     }
+
+    private fun checkGPS() {
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(this@MainActivity, "GPS is enable", Toast.LENGTH_LONG).show()
+        } else {
+            val locationDialog = MaterialAlertDialogBuilder(this@MainActivity)
+            locationDialog.setTitle("Attention")
+            locationDialog.setMessage("Location settings must be enabled from the settings to use the application")
+            locationDialog.setCancelable(false)
+            locationDialog.setPositiveButton(
+                "Open settings"
+            ) { _, _ ->
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+            locationDialog.create().show()
+        }
+    }
+
+    private fun isLocationPermissionGranted(): Boolean {
+        return if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                109
+            )
+            checkGPS()
+            false
+        } else {
+            true
+        }
+    }
+
+
 }
